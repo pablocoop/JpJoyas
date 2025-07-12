eeb18d82906bd9b5dbd291e309e67e2cb91c1af2
Pablo Osorio <pabloosorionavarro@gmail.com>
2025-07-12 15:12:33 -0400

    deleted django try

--- catalogo/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProductoListView.as_view(), name='home'),
-]
--- catalogo/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from .models import Producto
-
-class ProductoListView(ListView):
-    model               = Producto
-    template_name       = "catalogo/producto_list.html"
-    context_object_name = "productos"
-    paginate_by         = 12    # opcional: paginación
